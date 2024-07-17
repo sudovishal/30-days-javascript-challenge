1d1f5d5fc5ecfebe8191f904bef43bbd8c06c728
Vishal Shinde <shinde.vishal1809@gmail.com>
2024-07-17 19:20:54 +0530

    day five

--- day-five.js
+++ day-five.js
@@ -1,3 +1,4 @@
+// Activity 1
 function checkEvenOdd(number) {
     if (number % 2 === 0) {
         console.log(number + " is even");
@@ -5,15 +6,12 @@ function checkEvenOdd(number) {
         console.log(number + " is odd");
     }
 }
-
-
-checkEvenOdd(4);  // Output: 4 is even
+checkEvenOdd(4); 
 checkEvenOdd(7); 
 
 function squareOfNumber(number) {
     return number * number;
 }
-
 console.log(squareOfNumber(4));
 
 // Activity 2
@@ -26,7 +24,6 @@ const findMax = function(a, b) {
         console.log(a + " and " + b + " are equal");
     }
 };
-
 findMax(10, 20); 
 findMax(30, 15);  
 findMax(25, 25);
@@ -34,9 +31,51 @@ findMax(25, 25);
 const concatenateStrings = function(str1, str2) {
     return str1 + str2;
 };
-
 console.log(concatenateStrings("Hello, ", "world!")); 
 console.log(concatenateStrings("JavaScript ", "Function Expressions")); 
 
 // Activity 3
-console.log((a, b) => a + b);
+const sum = (a, b) => a + b;
+console.log(sum(10, 20));
+
+const containsCharacter = (str, char) => str.includes(char);
+console.log(containsCharacter("hello", "e")); 
+
+// Activity 4
+function multiply(a, b = 1) {
+    return a * b;
+  }
+  console.log(multiply(5, 2)); 
+  console.log(multiply(5)); 
+  
+function greet(name, age = 20) {
+    return "Hello, " + name + "! You seem to be " + age + " years old.";
+  }
+  
+  console.log(greet("Vishal")); 
+  console.log(greet("Vincent", 22)); 
+
+// Activity 5
+function repeatFunction(func, n) {
+    return function(x) {
+        let result = x;
+        for (let i = 0; i < n; i++) {
+            result = func(result);
+        }
+        return result;
+    };
+}
+const double = x => x * 2;
+const quadruple = repeatFunction(double, 9);
+console.log(quadruple(3)); // Output: 12
+
+function composeFunctions(func1, func2) {
+    return function(x) {
+        return func2(func1(x));
+    };
+}
+
+const add1 = x => x + 1;
+const square = x => x * x;
+const addThenSquare = composeFunctions(add1, square);
+console.log(addThenSquare(3));
